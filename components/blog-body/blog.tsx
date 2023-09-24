f581d5b715bb3c0dae59013c5e676a71d01d95c7
Mitko <mitkoiliev505@gmail.comm>
2023-09-24 18:48:34 +0200

    each blog description add

--- components/blog-body/blog.tsx
+++ components/blog-body/blog.tsx
@@ -1,15 +1,34 @@
+import ImageComponent from '../image/ImageComponent';
+import styles from './blogbody.module.css';
 type BlogProps = {
   place: {
     name: string;
     photoUrl: string;
     description: string;
+    review: string;
   };
 };
 export default function Blog({ place }: BlogProps) {
   return (
     <div>
-      <p>{place.description}</p>
-      <h2>Puta madre</h2>
+      <h1 className={styles.title}>{place.name}</h1>
+      <p className={styles.text}>{place.description}</p>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '500px',
+          marginTop: '2rem',
+        }}
+      >
+        <ImageComponent src={place.photoUrl} alt='' className={styles.image} />
+      </div>
+      <div style={{ paddingTop: '1rem' }}>
+        <h3 style={{ paddingTop: '1rem' }}>
+          Here are some key aspects of {place.name}:
+        </h3>
+        <p className={styles.text}>{place.review}</p>
+      </div>
     </div>
   );
 }
--- components/blog-info/blog-info.tsx
+++
@@ -1,4 +0,0 @@
-import styles from './blog-info.module.css';
-export default function BlogInfo() {
-  return <div>BlogInfo</div>;
-}
